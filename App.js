c2c0ff701022bd1b6e9afb32f7c11a9c13ede352
sungalex <cloudallera@gmail.com>
2021-01-18 18:44:01 +0900

    getting the weather with axios

--- App.js
+++ App.js
@@ -1,6 +1,40 @@
 import React from "react";
+import { Alert } from "react-native";
+import * as Location from "expo-location";
+import axios from "axios";
 import Loading from "./Loading";
+import { WEATHER_KEY } from "./secret";
 
-export default function App() {
-  return <Loading />;
+const API_KEY = WEATHER_KEY;
+
+export default class extends React.Component {
+  state = {
+    isLoading: true,
+  };
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+    );
+    console.log(data);
+  };
+  getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      console.log(latitude, longitude);
+      this.getWeather(latitude, longitude);
+      this.setState({ isLoading: false });
+    } catch (error) {
+      Alert.alert("Can't find you.", "So Sad");
+    }
+  };
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
+  }
 }
